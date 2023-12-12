caa437242358b4ad218e9be5550f160783a910bf
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-12-12 15:39:18 -0500

    Update InPlaceMergeSort.hpp

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -4,14 +4,21 @@
 #include <chrono>
 #ifndef INPLACEMERGESORT_HPP
 #define INPLACEMERGESORT_HPP
-
+/*
+CSCI335 Fall 2023
+Assignment 2 – InPlaceMergeSort
+WeiLin Gao
+12/12/2023
+InPlaceMergeSort.hpp: Finding the median using InPlaceMergeSort
+Same as merge sort, except use std::inplace_merge.
+*/
 void mergeSortHelp1(std::vector<int>& nums, int& duration, std::vector<int>::iterator start, std::vector<int>::iterator end)
 {
 	if (std::distance(start, end) > 1)
 	{
 		auto mid = start + distance(start, end) / 2;                 // Calculates the midpoint of the current range to divide it into two halves.
 		mergeSortHelp1(nums, duration, start, mid);                 // Recursively calls mergeSortHelp1 to sort the left and right halves of the range.
-		mergeSortHelp1(nums, duration, mid, end);
+        mergeSortHelp1(nums, duration, mid, end);
 		std::inplace_merge(start, mid, end);                       // Performs an in-place merge to merge the sorted left and right halves back into the original range.
 	}
 	
@@ -27,7 +34,8 @@ int inPlaceMergeSort(std::vector<int>& nums, int& duration)
 
 	auto endTime = std::chrono::steady_clock::now(); 
 
-	 duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+	std::chrono::duration<double, std::micro> elapsed = endTime - startTime;
+	duration = static_cast<int>(elapsed.count());
 
 
 	// Returns the median or the element before it for even-sized vectors.
