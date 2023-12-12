810b958bd13001ded64b2a7338e39ab91fc04c9a
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-12-12 14:58:41 -0500

    Update QuickSelect.hpp

--- QuickSelect.hpp
+++ QuickSelect.hpp
@@ -7,40 +7,11 @@
 
 std::vector<int>::iterator medianofthree(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high, std::vector<int>::iterator mid)
 {
-	if (*low > *mid && *low > *high && *mid > *low)                   
-	{                                   
-		return mid;
-	}
-	                                                                   //  Find the median of the chosen elements(first element, median element, and last element)
-	else if (*low > *mid && *low > *high && *high > *mid)                     
-	{
-		return high;                                                   //comparing the 3 elements and return the selected element that fulfil the condition
-
-	}                                                                  // The chosen element will be the pivot for hoare partitioon
-	else if (*low > *mid && *low < *high && *high > *mid)
-	{
-		return low;
-	}
-	else if (*low < *mid && *low > *high && *high < *mid)
-	{
-		return low;
-	}
-	else if (*low < *mid && *low < *high && *high < *mid)
-	{
-		return high;
-	}
-
-	else if (*low < *mid && *low < *high && *high > *mid)
-	{
-		return mid;
-	}
-
-	else
-	{
-		return mid;
-	}
-	
+	int arr[] = { *low, *mid, *high };
+	std::sort(std::begin(arr), std::end(arr));
+	return std::find(nums.begin(), nums.end(), arr[1]);
 }
+
 std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high)
 {
 	auto mid = low + std::distance(low, high) / 2;                  // declare an median element and pass it into medianofthree function
@@ -102,6 +73,8 @@ int quickSelect1(std::vector<int>& nums, std::vector<int>::iterator low, std::ve
 			low = pivot + 1;
 		}
 	}
+
+	return -1;
 
 
 }
