52e4fc6fad1cc0092d30f7269b48ed8cdb7369f1
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-12-12 15:40:03 -0500

    Update WorstCaseQuickSelect.hpp

--- WorstCaseQuickSelect.hpp
+++ WorstCaseQuickSelect.hpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+#include <chrono>
 #include "QuickSelect.hpp"
 
 #ifndef WORSTCASEQUICKSELECT_HPP
 #define WORSTCASEQUICKSELECT_HPP
 
-
+/*
+CSCI335 Fall 2023
+Assignment 2 – WorstCaseQuickSelect
+WeiLin Gao
+12/12/2023
+WorstCaseQuickSelect.hpp: creating a worse case input and test it on quickselect
+*/
 std::vector<int> worstCaseQuickSelect()
 {
     int duration;
+
     std::vector<int> worstCaseQuickSelect;
-    for (int i = 1; i <= 20000; ++i) 
+   
+    for (int i = 1; i <= 20000; ++i)              // fill the vector with value from 1-20000
     {
         worstCaseQuickSelect.push_back(i);
     }
